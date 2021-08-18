0de09c0a9681013e31bb4c7715508a30e6988212
Choi semi <cyhse7@gmail.com>
2021-08-18 21:38:51 +0900

    Update form.js

--- js/form.js
+++ js/form.js
@@ -22,7 +22,7 @@ function a() {
     result[0] += num;
 
     console.log('선택한 값 : ' + $(":input:radio[name=anser_1]:checked").val());
-    console.log('a 값 : ' + result);
+    console.log('a 값 : ' + result[0]);
 }
 
 // 자기수용/피드백
@@ -32,7 +32,7 @@ function b() {
     result[1] += num;
 
     console.log('선택한 값 : ' + $(":input:radio[name=anser_2]:checked").val());
-    console.log('a 값 : ' + result);
+    console.log('b 값 : ' + result[1]);
 }
 
 // 자존감
@@ -42,7 +42,7 @@ function c() {
     result[2] += num;
 
     console.log('선택한 값 : ' + $(":input:radio[name=anser_3]:checked").val());
-    console.log('a 값 : ' + result);
+    console.log('c 값 : ' + result[2]);
 }
 
 $(function () {
@@ -62,47 +62,50 @@ $(function () {
         scrollUp();
     });
 
+    $('html, body').animate({
+        scrollTop: 0
+    }, 500);
+});
+
+$(function () {
     $('#form').submit(function (e) {
         if(7 <= result[0] <= 21){
             if(6 <= result[1] <= 18){
                 if(10 <= result[2] <= 30){
-                    //location.href = "../result/거북이";
+                    location.href = "../result/거북이";
                     console.log("거북이");
                 } else if(31 <= result[2] <=50){
-                    //location.href = "../result/고슴도치";
+                    location.href = "../result/고슴도치";
                     console.log("고슴도치");
                 }
             } else if(19 <= result[1] <= 30){
                 if(10 <= result[2] <= 30){
-                    //location.href = "../result/토끼";
+                    location.href = "../result/토끼";
                     console.log("토끼");
                 } else if(31 <= result[2] <=50){
-                    //location.href = "../result/사자";
+                    location.href = "../result/사자";
                     console.log("사자");
                 }
             }
         } else if(22 <= result[0] <= 35){
             if(6 <= result[1] <= 18){
                 if(10 <= result[2] <= 30){
-                    //location.href = "../result/플라밍고";
+                    location.href = "../result/플라밍고";
                     console.log("플라밍고");
                 } else if(31 <= result[2] <=50){
-                    //location.href = "../result/코뿔소";
+                    location.href = "../result/코뿔소";
                     console.log("코뿔소");
                 }
             } else if(19 <= result[1] <= 30){
                 if(10 <= result[2] <= 30){
-                    //location.href = "../result/미어캣";
+                    location.href = "../result/미어캣";
                     console.log("미어캣");
                 } else if(31 <= result[2] <=50){
-                    //location.href = "../result/고래";
+                    location.href = "../result/고래";
                     console.log("고래");
                 }
             }
         }
+        return false;
     });
-
-    $('html, body').animate({
-        scrollTop: 0
-    }, 500);
 });
